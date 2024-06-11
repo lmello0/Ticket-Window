2b935c0edc9230ab54b7e8bcb48ee894346858c8
lmello <melloluc02@gmail.com>
2024-06-11 13:17:14 -0300

    :recycle: refactor: Move logic to service package

--- frontdoor/src/main/java/br/com/lmello/frontdoor/controller/CustomerController.java
+++ frontdoor/src/main/java/br/com/lmello/frontdoor/controller/CustomerController.java
@@ -3,7 +3,7 @@ package br.com.lmello.frontdoor.controller;
 import br.com.lmello.common.customer.CustomerDTO;
 import br.com.lmello.common.workshift.WorkHourChecker;
 import br.com.lmello.frontdoor.dto.MessageDTO;
-import br.com.lmello.frontdoor.kafka.CustomerProducer;
+import br.com.lmello.frontdoor.service.CustomerService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,19 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class CustomerController {
 
-    private final CustomerProducer customerProducer;
+    private final CustomerService customerService;
 
-    public CustomerController(CustomerProducer customerProducer) {
-        this.customerProducer = customerProducer;
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
     }
 
     @PostMapping
     public ResponseEntity<MessageDTO> addToQueue(@RequestBody @Valid CustomerDTO customer) {
         if (!WorkHourChecker.isInWorkHour()) {
-            return ResponseEntity.badRequest().body(new MessageDTO("You are not in work hour!"));
+            return ResponseEntity.badRequest().body(new MessageDTO("We are not in work hour!"));
         }
 
-        customerProducer.sendMessage(customer);
+        customerService.sendMessage(customer);
 
         return ResponseEntity.ok(new MessageDTO("You are in the queue, and must be attended in a few moments"));
     }
--- frontdoor/src/main/java/br/com/lmello/frontdoor/service/CustomerService.java
+++ frontdoor/src/main/java/br/com/lmello/frontdoor/service/CustomerService.java
@@ -1,4 +1,4 @@
-package br.com.lmello.frontdoor.kafka;
+package br.com.lmello.frontdoor.service;
 
 import br.com.lmello.common.customer.CustomerDTO;
 import br.com.lmello.common.customer.UnprocessedCustomerDTO;
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Service;
 import java.util.logging.Logger;
 
 @Service
-public class CustomerProducer {
-    private final Logger logger = Logger.getLogger(CustomerProducer.class.getName());
+public class CustomerService {
+    private final Logger logger = Logger.getLogger(CustomerService.class.getName());
 
     private final KafkaTemplate<String, UnprocessedCustomerDTO> kafkaTemplate;
 
-    public CustomerProducer(KafkaTemplate<String, UnprocessedCustomerDTO> kafkaTemplate) {
+    public CustomerService(KafkaTemplate<String, UnprocessedCustomerDTO> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
