3fa10c34d77752f070a6bdece78f645a6399da26
lmello <melloluc02@gmail.com>
2024-06-12 17:23:18 -0300

    :recycle: refactor: Add endpoint to frontdoor

--- frontdoor/src/main/java/br/com/lmello/frontdoor/controller/CustomerController.java
+++ frontdoor/src/main/java/br/com/lmello/frontdoor/controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/queue")
 public class CustomerController {
 
     private final CustomerService customerService;
